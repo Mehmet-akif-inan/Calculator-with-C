65d377d08f44385cdb1aa8d1f58576d9ac1a40c3
Mehmet Akif Inan <72706177+Mehmet-akif-inan@users.noreply.github.com>
2021-10-11 18:10:19 +0300

    Create Calculator with C.cpp
    I made a simple calculator with C

---
+++ Calculator with C.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+char Akif(char a){
+ 
+scanf("%c",&a);
+ 
+return a;
+}
+ 
+int main(){
+int b,c,d,e;
+char a;
+char i;
+i=Akif(a);
+printf("Enter two numbers\n");
+scanf("%d",&b);
+scanf("%d",&c);
+ 
+if(i=='+'){
+ 
+d=b+c;
+ 
+printf("%d",d);
+ 
+}
+ 
+else if(i=='-'){ d=b-c;
+ 
+printf("%d",d);
+ 
+}
+ 
+else if(i=='*'){ d=b*c;
+ 
+printf("%d",d);
+ 
+}
+else if(i=='/'){ d=b/c;
+ 
+printf("%d",d);
+ 
+} 
+getch ();
+ 
+return 0;
+ 
+}
